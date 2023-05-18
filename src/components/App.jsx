e40c89aa2bcec21991d67fcbf3634a13b0895486
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2023-05-18 08:15:22 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,6 @@ import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Notification } from './Notification/Notification';
 
-// class Counter extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
 export default function Counter() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -18,32 +11,18 @@ export default function Counter() {
 
   const feedbackStatus = { good, neutral, bad };
 
-  // onLeaveFeedback = options => {
-  //   this.setState({ [options]: this.state[options] + 1 });
-  // };
-
-  // countTotalFeedback = () => {
-  //   const { good, neutral, bad } = this.state;
-  //   const total = good + neutral + bad;
-  //   return total;
-  // };
-
-  //   const countTotalFeedback = () => {
-  //   return good + neutral + bad;
-  // }
-
   const onLeaveFeedback = options => {
     switch (options) {
       case 'good':
-        setGood(good + 1);
+        setGood(state => state + 1);
         break;
 
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(state => state + 1);
         break;
 
       case 'bad':
-        setBad(bad + 1);
+        setBad(state => state + 1);
         break;
 
       default:
